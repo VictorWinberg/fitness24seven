4ffe6334f862821fad8c017a81fcb2902d696333
Victor Winberg <victor.winberg@jayway.com>
2020-10-13 15:43:26 +0200

    Add debugging

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const url = "https://se.fitness24seven.com/mina-sidor/oversikt/";
 const headless = true;
 
+/** @param {number} ms */
 function sleep(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
@@ -12,121 +13,133 @@ function sleep(ms) {
 }
 
 /**
- * Books a bodypump session in 2 days on fitness24seven
+ * Books a session on fitness24seven
  * @param {string} gym
  */
 async function bookSession(gym) {
-    const browser = await puppeteer.launch({
-    //   defaultViewport: null,
-    //   headless,
-      executablePath: "chromium-browser",
-    });
-    const page = await browser.newPage();
+    let browser, page;
+    try {
+        console.log(`Booking ${gym}...`);
+        browser = await puppeteer.launch({
+            defaultViewport: { width: 1200, height: 800 },
+            headless,
+            executablePath: "chromium-browser",
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+        page = await browser.newPage();
 
-    await page.setViewport({ width: 1200, height: 2000 });
+        page.on("console", (msg) => console.log("\x1b[33mCONSOLE\x1b[0m", msg.text()));
 
-    page.on("console", (msg) => console.log("\x1b[33mCONSOLE\x1b[0m", msg.text()));
+        // Homepage
+        await page.goto(url);
+        console.log(' --Homepage');
 
-    // Homepage
-    await page.goto(url, {
-      waitUntil: "load",
-      // Remove the timeout
-      timeout: 0,
-    });
+        // Go to login
+        await page.waitForSelector(".c-login .c-btn");
+        await page.evaluate(() => { document.querySelector(".c-login .c-btn").click() });
+        console.log(' --Login');
 
-    // Go to login
-    await page.waitForSelector(".c-login .c-btn");
-    await page.evaluate(() => { document.querySelector(".c-login .c-btn").click() });
+        // Login
+        await page.waitForSelector("#logonIdentifier");
+        await page.evaluate(
+            (email, pass) => {
+                document.getElementById("logonIdentifier").value = email;
+                document.getElementById("password").value = pass;
+                document.getElementById("next").click();
+            },
+            process.env.USER_1_EMAIL,
+            process.env.USER_1_PASSWORD
+        );
 
-    // Login
-    await page.waitForSelector("#logonIdentifier");
-    await page.evaluate(
-        (email, pass) => {
-            document.getElementById("logonIdentifier").value = email;
-            document.getElementById("password").value = pass;
-            document.getElementById("next").click();
-        },
-        process.env.USER_1_EMAIL,
-        process.env.USER_1_PASSWORD
-    );
+        // Go to booking
+        // await page.waitForSelector(".c-info-box--cta");
+        // await page.evaluate(() => document.querySelector(".c-info-box--cta").click());
+        await page.waitForSelector(".c-arrow-cta__link[href='/mina-sidor/boka-grupptraning/']");
+        await page.evaluate(() => document.querySelector(".c-arrow-cta__link[href='/mina-sidor/boka-grupptraning/']").click());
+        console.log(' --Booking');
 
-    // Go to booking
-    // await page.waitForSelector(".c-info-box--cta");
-    // await page.evaluate(() => document.querySelector(".c-info-box--cta").click());
-    await page.waitForSelector(".c-arrow-cta__link[href='/mina-sidor/boka-grupptraning/']");
-    await page.evaluate(() => document.querySelector(".c-arrow-cta__link[href='/mina-sidor/boka-grupptraning/']").click());
+        // Helper methods for filter selecting
+        const filterSelector = (id, dd) => `.u-display-none--sm .c-class-filter:nth-child(${id}) .c-filter-dropdown:nth-child(${dd}) .c-filter-dropdown__button--clickable`;
+        await page.evaluate(() => {
+            window.filterSelector = function (id, dd) {
+                return `.u-display-none--sm .c-class-filter:nth-child(${id}) .c-filter-dropdown:nth-child(${dd}) .c-filter-dropdown__button--clickable`;
+            };
+        });
 
-    // Helper methods for filter selecting
-    const filterSelector = (id, dd) => `.u-display-none--sm .c-class-filter:nth-child(${id}) .c-filter-dropdown:nth-child(${dd}) .c-filter-dropdown__button--clickable`;
-    await page.evaluate(() => {
-        window.filterSelector = function (id, dd) {
-            return `.u-display-none--sm .c-class-filter:nth-child(${id}) .c-filter-dropdown:nth-child(${dd}) .c-filter-dropdown__button--clickable`;
-        };
-    });
+        // Set Weekday
+        await page.waitForSelector(`.c-weekday-switcher__weekday-container:nth-child(${3})`);
+        await page.evaluate(() => document.querySelector(`.c-weekday-switcher__weekday-container:nth-child(${3})`).click());
 
-    // Set Weekday
-    await page.waitForSelector(`.c-weekday-switcher__weekday-container:nth-child(${3})`);
-    await page.evaluate(() => document.querySelector(`.c-weekday-switcher__weekday-container:nth-child(${3})`).click());
+        // Free spots
+        await page.waitForSelector(".c-filter-toggle__toggle-input");
+        await page.evaluate(() => document.querySelector(".c-filter-toggle__toggle-input").click());
 
-    // Free spots
-    await page.waitForSelector(".c-filter-toggle__toggle-input");
-    await page.evaluate(() => document.querySelector(".c-filter-toggle__toggle-input").click());
+        // Country
+        await page.waitForSelector(filterSelector(1, 2));
+        await page.evaluate(() => document.querySelector(window.filterSelector(1, 2)).click());
 
-    // Country
-    await page.waitForSelector(filterSelector(1, 2));
-    await page.evaluate(() => document.querySelector(window.filterSelector(1, 2)).click());
+        // Country Sweden
+        await page.waitForSelector("#checkbox-Sverige-input");
+        await page.evaluate(() => document.getElementById("checkbox-Sverige-input").click());
 
-    // Country Sweden
-    await page.waitForSelector("#checkbox-Sverige-input");
-    await page.evaluate(() => document.getElementById("checkbox-Sverige-input").click());
+        // City
+        await page.waitForSelector(filterSelector(1, 3));
+        await page.evaluate(() => document.querySelector(window.filterSelector(1, 3)).click());
 
-    // City
-    await page.waitForSelector(filterSelector(1, 3));
-    await page.evaluate(() => document.querySelector(window.filterSelector(1, 3)).click());
+        // City Malmo
+        await page.waitForSelector("#checkbox-Malmö-input");
+        await page.evaluate(() => document.getElementById("checkbox-Malmö-input").click());
 
-    // City Malmo
-    await page.waitForSelector("#checkbox-Malmö-input");
-    await page.evaluate(() => document.getElementById("checkbox-Malmö-input").click());
+        // Gym
+        await page.waitForSelector(filterSelector(1, 4));
+        await page.evaluate(() => document.querySelector(window.filterSelector(1, 4)).click());
 
-    // Gym
-    await page.waitForSelector(filterSelector(1, 4));
-    await page.evaluate(() => document.querySelector(window.filterSelector(1, 4)).click());
+        switch (gym.toLowerCase()) {
+            case "lilla torg":
+                // Gym Malmo Lilla Torg
+                await page.waitForSelector("[id='checkbox-Malmö Lilla Torg-input']");
+                await page.evaluate(() => document.getElementById("checkbox-Malmö Lilla Torg-input").click());
+                break;
+            case "katrinelund":
+                // Gym Malmö Katrinelund
+                await page.waitForSelector("[id='checkbox-Malmö Katrinelund-input']");
+                await page.evaluate(() => document.getElementById("checkbox-Malmö Katrinelund-input").click());
+                break;
+            case "dalaplan":
+                // Gym Malmö Dalaplan
+                await page.waitForSelector("[id='checkbox-Malmö Dalaplan-input']");
+                await page.evaluate(() => document.getElementById("checkbox-Malmö Dalaplan-input").click());
+                break;
+        }
 
-    switch (gym.toLowerCase()) {
-        case "lilla torg":
-            // Gym Malmo Lilla Torg
-            await page.waitForSelector("[id='checkbox-Malmö Lilla Torg-input']");
-            await page.evaluate(() => document.getElementById("checkbox-Malmö Lilla Torg-input").click());
-            break;
-        case "katrinelund":
-            // Gym Malmö Katrinelund
-            await page.waitForSelector("[id='checkbox-Malmö Katrinelund-input']");
-            await page.evaluate(() => document.getElementById("checkbox-Malmö Katrinelund-input").click());
-            break;
-        case "dalaplan":
-            // Gym Malmö Dalaplan
-            await page.waitForSelector("[id='checkbox-Malmö Dalaplan-input']");
-            await page.evaluate(() => document.getElementById("checkbox-Malmö Dalaplan-input").click());
-            break;
+        // Session
+        await page.waitForSelector(filterSelector(2, 2));
+        await page.evaluate(() => document.querySelector(window.filterSelector(2, 2)).click());
+
+        // Session Bodypump
+        await page.waitForSelector("#checkbox-BODYPUMP®-input");
+        await page.evaluate(() => document.getElementById("checkbox-BODYPUMP®-input").click());
+
+        console.log(' --Looking for available session');
+
+        // Book
+        await page.waitForSelector(".c-class-card__button:not(.c-btn--cancel)");
+        await page.evaluate(() => document.querySelector(".c-class-card__button:not(.c-btn--cancel)").click());
+
+        console.log("Booking completed " + new Date());
+
+        await sleep(10000);
+
+        await browser.close();
+
+        console.log('Browser closed');
+
+    } catch (error) {
+        console.error(error);
+        const html = await page.evaluate(() => document.body.innerHTML)
+        console.log(html.replace(/\n/g, ""));
+        await browser.close();
     }
-
-    // Session
-    await page.waitForSelector(filterSelector(2, 2));
-    await page.evaluate(() => document.querySelector(window.filterSelector(2, 2)).click());
-
-    // Session Bodypump
-    // await page.waitForSelector("#checkbox-BODYPUMP®-input");
-    // await page.evaluate(() => document.getElementById("checkbox-BODYPUMP®-input").click());
-
-    // Book
-    await page.waitForSelector(".c-class-card__button:not(.c-btn--cancel)");
-    await page.evaluate(() => document.querySelector(".c-class-card__button:not(.c-btn--cancel)").click());
-
-    await sleep(10000);
-
-    await browser.close();
-
-    console.log("Booking completed " + new Date());
 }
 
 // *    *    *    *    *    *
@@ -151,18 +164,19 @@ function schedule(weekday, hours, minutes, gym) {
     new CronJob(
         `0 ${minutes} ${hours} * * ${_weekday}`,
         function () {
-            console.log("Booking " + weekday + " at " + gym + " initiated " + new Date());
+            console.log(`Booking ${weekday} at ${gym} initiated ${new Date()}`);
             bookSession(gym)
         },
         null,
         true,
         "Europe/Berlin"
     ).start();
+
+    const [_hours, _minutes] = [String(hours).padStart(2, "0"), String(minutes).padStart(2, "0")];
+    console.log(`Scheduling booking ${_hours}:${_minutes} for ${weekday} at ${gym}`);
 }
 
-schedule("wednesday", 06, 30, "Lilla Torg")
-schedule("wednesday", 18, 15, "Dalaplan")
-schedule("thursday", 18, 00, "Katrinelund")
-schedule("thursday", 19, 00, "Lilla Torg")
-
-bookSession("Katrinelund")
+schedule("wednesday", 06, 30, "Lilla Torg");
+schedule("wednesday", 18, 15, "Dalaplan");
+schedule("thursday", 18, 00, "Katrinelund");
+schedule("thursday", 19, 00, "Lilla Torg");
