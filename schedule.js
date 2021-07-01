52a021b465f2df0bd60efaf2321b01e7287d9582
Catarina Sörensen <catarinasorensen1991@gmail.com>
2021-07-01 10:33:27 +0200

    Adds CS to schedule

--- schedule.js
+++ schedule.js
@@ -1,6 +1,8 @@
 module.exports = (schedule, Day, User, Gym) => {
   // schedule(Day.Monday, "18", "00", User.X, Gym.Katrinelund);
   // schedule(Day.Monday, "20", "00", User.X, Gym.Dalaplan);
+  
+  schedule(Day.Tuesday, "17", "00", User.CS, Gym.Dalaplan);
 
   schedule(Day.Wednesday, "06", "30", User.VW, Gym.Lilla_Torg);
   schedule(Day.Wednesday, "06", "30", User.AO, Gym.Lilla_Torg);
@@ -13,9 +15,14 @@ module.exports = (schedule, Day, User, Gym) => {
   schedule(Day.Thursday, "12", "00", User.AO, Gym.Lilla_Torg);
   // schedule(Day.Thursday, "18", "00", User.X, Gym.Katrinelund);
   // schedule(Day.Thursday, "19", "00", User.X, Gym.Lilla_Torg);
+  schedule(Day.Thursday, "18", "00", User.CS, Gym.Dalaplan);
 
   // schedule(Day.Saturday, "10", "00", User.X, Gym.Lilla_Torg);
   // schedule(Day.Saturday, "10", "00", User.X, Gym.Lilla_Torg);
+    schedule(Day.Saturday, "10", "00", User.CS, Gym.Lilla_Torg);
+    schedule(Day.Saturday, "12", "15", User.CS, Gym.Varnhem);
+
+  
 
   // schedule(Day.Sunday, "10", "00", User.X, Gym.Lilla_Torg);
   // schedule(Day.Sunday, "10", "00", User.X, Gym.Lilla_Torg);
