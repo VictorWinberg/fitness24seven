82b2d3f045a646554d5ab29a06fa59ef62ff4993
Victor Winberg <victor.m.winberg@gmail.com>
2021-07-03 18:32:01 +0200

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -7,22 +7,13 @@ module.exports = (schedule, Day, User, Workout, Gym) => {
   schedule(Day.Wednesday, "06", "30", User.VW, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Wednesday, "06", "30", User.AO, Workout.Bodypump, Gym.Lilla_Torg);
   // schedule(Day.Wednesday, "06", "30", User.AG, Workout.Bodypump, Gym.Lilla_Torg);
-  // schedule(Day.Wednesday, "06", "30", User.X, Workout.Bodypump, Gym.Lilla_Torg);
-  // schedule(Day.Wednesday, "18", "15", User.X, Workout.Bodypump, Gym.Dalaplan);
-
 
   schedule(Day.Thursday, "12", "00", User.VW, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Thursday, "12", "00", User.AO, Workout.Bodypump, Gym.Lilla_Torg);
-  // schedule(Day.Thursday, "18", "00", User.X, Workout.Bodypump, Gym.Katrinelund);
-  // schedule(Day.Thursday, "19", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Thursday, "18", "00", User.CS, Workout.Bodypump, Gym.Dalaplan);
 
-
-  // schedule(Day.Saturday, "10", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
-  // schedule(Day.Saturday, "10", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Saturday, "10", "00", User.CS, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Saturday, "12", "15", User.CS, Workout.Bodypump, Gym.Varnhem);
-
   
   // schedule(Day.Sunday, "10", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
   // schedule(Day.Sunday, "10", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
