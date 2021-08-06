0f601cfbfe8730fd69a1c440e3bef2ee1b4ff6dd
Catarina Sörensen <catarina.sorensen@jayway.com>
2021-08-06 15:26:16 +0200

    Temporary removes bookings

--- schedule.js
+++ schedule.js
@@ -2,16 +2,15 @@ module.exports = (schedule, Day, User, Workout, Gym) => {
   // schedule(Day.Monday, "18", "00", User.X, Workout.Bodypump, Gym.Katrinelund);
   // schedule(Day.Monday, "20", "00", User.X, Workout.Bodypump, Gym.Dalaplan);
   
-  schedule(Day.Tuesday, "17", "00", User.CS, Workout.Bodystep, Gym.Dalaplan);
+  //schedule(Day.Tuesday, "17", "00", User.CS, Workout.Bodystep, Gym.Dalaplan);
 
   schedule(Day.Wednesday, "06", "30", User.VW, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Wednesday, "06", "30", User.AO, Workout.Bodypump, Gym.Lilla_Torg);
   // schedule(Day.Wednesday, "06", "30", User.AG, Workout.Bodypump, Gym.Lilla_Torg);
 
-  schedule(Day.Thursday, "18", "00", User.CS, Workout.Bodystep, Gym.Dalaplan);
+  // schedule(Day.Thursday, "18", "00", User.CS, Workout.Bodystep, Gym.Dalaplan);
 
-  schedule(Day.Saturday, "10", "00", User.CS, Workout.Bodypump, Gym.Lilla_Torg);
-  schedule(Day.Saturday, "12", "15", User.CS, Workout.Bodystep, Gym.Varnhem);
+  // schedule(Day.Saturday, "12", "15", User.CS, Workout.Bodystep, Gym.Varnhem);
   
   // schedule(Day.Sunday, "10", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
   // schedule(Day.Sunday, "10", "00", User.X, Workout.Bodypump, Gym.Lilla_Torg);
