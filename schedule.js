634953c9f2b16c6bc3077d72750fc8ba9c01b235
Victor Winberg <victor.m.winberg@gmail.com>
2022-01-10 18:27:36 +0100

    Remove test-box from schedule

--- schedule.js
+++ schedule.js
@@ -6,7 +6,6 @@ module.exports = ({ schedule, Day, User, Workout, Gym }) => {
 
   schedule(Day.Wednesday, "06", "30", User.VW, Workout.Bodypump, Gym.Lilla_Torg);
   schedule(Day.Wednesday, "06", "30", User.AO, Workout.Bodypump, Gym.Lilla_Torg);
-  schedule(Day.Wednesday, "18", "10", User.VW, Workout.Box, Gym.Lilla_Torg);
   // schedule(Day.Wednesday, "06", "30", User.AG, Workout.Bodypump, Gym.Lilla_Torg);
 
   schedule(Day.Thursday, "18", "20", User.VW, Workout.Box, Gym.Lilla_Torg);
