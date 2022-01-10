8c1c8eb601607bb738cf638c2aa18bc69cd2031f
Victor Winberg <victor.winberg@jayway.com>
2022-01-10 08:32:03 +0100

    Refactor code to ui and server

--- constants.js
+++ constants.js
@@ -16,17 +16,18 @@ const User = {
 }
 
 const Gym = {
-    "Lilla_Torg": "malmo-lilla-torg",
-    "Dalaplan": "malmoe-dalaplan",
-    "Katrinelund": "malmoe-katrinelund",
-    "Varnhem": "malmoe-vaernhem",
+    "Lilla_Torg": { var: "malmo-lilla-torg", name: "Malmö Lilla Torg" },
+    "Dalaplan": { var: "malmoe-dalaplan", name: "Malmö Dalaplan" },
+    "Katrinelund": { var: "malmoe-katrinelund", name: "Malmö Katrinelund" },
+    "Varnhem": { var: "malmoe-vaernhem", name: "Malmö Värnhem" },
 }
 
 const Workout = {
-    "Bodypump": "bodypump",
-    "Bodystep": "bodystep",
-    "Bodycombat": "bodycombat",
-    "Bodyattack": "bodyattack",
+    "Bodypump": { var: "bodypump", name: "BODYPUMP®" },
+    "Bodystep": { var: "bodystep", name: "BODYSTEP®" },
+    "Bodycombat": { var: "bodycombat", name: "BODYCOMBAT®" },
+    "Bodyattack": { var: "bodyattack", name: "BODYATTACK®" },
+    "Box": { var: "box", name: "BOX" },
 }
 
 module.exports = { Day, User, Gym, Workout }
---
+++ ui.js
@@ -0,0 +1,17 @@
+const { User, Gym, Workout } = require("./constants.js");
+
+require("./cron")({
+  puppeteerOptions: {
+    defaultViewport: { width: 1200, height: 800 },
+    headless: false,
+    executablePath: "",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  notify: false,
+  testBook: {
+    date: "2022-01-11 20:15",
+    user: User.VW,
+    workout: Workout.Box,
+    gym: Gym.Dalaplan,
+  },
+});
